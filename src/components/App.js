dbc8263a4746be89c7b0b04dc4fae335f23556b6
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-11-26 15:27:57 -0800

    finishing framework setup for shopping cart

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import "../styles/App.css"
 import MainPage from "./MainPage"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
-export default props => {
+const App = props => {
   return (
     <Router>
       <Route exact path="/" component={MainPage} />
     </Router>
   )
 }
+
+export default App
